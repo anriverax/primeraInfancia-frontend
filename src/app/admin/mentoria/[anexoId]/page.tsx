db688e91221726f55086b266e8e569f9d7a5a67e
Jaime Pajares <jaimepajares@gmail.com>
2025-09-15 23:19:00 -0600

    feat  add appendix 3

--- src/app/admin/mentoria/[anexoId]/page.tsx
+++ src/app/admin/mentoria/[anexoId]/page.tsx
@@ -3,23 +3,24 @@
 import { useParams } from "next/navigation";
 import { useAttachment1Form } from "@/features/attachment/instrument1/useAttachmentForm";
 import { useAttachment2Form } from "@/features/attachment/instrument2/useAttachmentForm";
+import { useAttachment3Form } from "@/features/attachment/instrument3/useAttachmentForm";
 import Attachment1Form from "@/features/attachment/instrument1/components/attachment";
 import Attachment2Form from "@/features/attachment/instrument2/components/attachment";
+import Attachment3Form from "@/features/attachment/instrument3/components/attachment";
 const AttachmentDetailPage = (): React.JSX.Element => {
-  const params = useParams();
+    const params = useParams();
 
-  const formikAttachment1 = useAttachment1Form();
-  const formikAttachment2 = useAttachment2Form();
+    const formikAttachment1 = useAttachment1Form();
+    const formikAttachment2 = useAttachment2Form();
+    const formikAttachment3 = useAttachment3Form();
+    return (
+        <>
+            {Number(params.anexoId) === 1 && <Attachment1Form formik={formikAttachment1} />}
 
-  // const { status, errors, setStatus } = formikAttachment1;
-  // const { status, errors, setStatus } = formikAttachment2;
+            {Number(params.anexoId) === 2 && <Attachment2Form formik={formikAttachment2} />}
 
-  return (
-    <>
-      {Number(params.anexoId) === 1 && <Attachment1Form formik={formikAttachment1} />}
-
-      {Number(params.anexoId) === 2 && <Attachment2Form formik={formikAttachment2} />}
-    </>
-  );
+            {Number(params.anexoId) === 3 && <Attachment3Form formik={formikAttachment3} />}
+        </>
+    );
 };
 export default AttachmentDetailPage;
---
+++ src/features/attachment/instrument3/type.ts
@@ -0,0 +1,36 @@
+import { AxiosMessage } from "@/shared/types/globals";
+
+export interface Attachment3Input {
+  teacherName: string;
+  mentorName: string;
+  schoolName: string;
+  departmentMunicipality: string;
+  startDate: string;
+  goalList: string;
+  workArea: string;
+  justification: string;
+  priorityLevel: string;
+  plannedDate: string;
+  activity: string;
+  mode: string;
+  responsible: string;
+  observations: string;
+  classrromObservations: string;
+  dialoguedFeedback: string;
+  modelingPractices: string;
+  coPlanningActivities: string;
+  portfolioReview: string;
+  analysisEvidence: string;
+  other: string;
+  resourceList: string;
+  expectedIndicators: string;
+  reviewFrecuency: string;
+  adjustedPlan: string;
+  teachingPortfolio: string;
+  observationRecords: string;
+  otherEvidence: string;
+  improveAspects: string;
+  proposals: string;
+}
+
+export type IAttachment3Input = Attachment3Input & AxiosMessage;
