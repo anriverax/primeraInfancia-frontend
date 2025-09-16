f46af9abab089c9c2f1144ad7bee2742a5207701
Jaime Pajares <jaimepajares@gmail.com>
2025-09-15 22:00:01 -0600

    feat add appendix page

--- src/app/admin/mentoria/page.tsx
+++ src/app/admin/mentoria/page.tsx
@@ -1,23 +1,19 @@
 "use client";
 import TrainerView from "@/features/mentoring/trainer/components/trainerView";
-import { TypeRole } from "@/shared/constants";
-import { useSession } from "next-auth/react";
 import { ListTodo } from "lucide-react";
 
 export default function MentoringPage(): React.JSX.Element {
-    const { data: session } = useSession();
-
-    return (
-        <div className="space-y-8">
-            <div className="flex w-full gap-3 justify-between">
-                <div className="flex items-center gap-2">
-                    <ListTodo className="h-5 w-5 text-blue-500" />
-                    <h2 className="text-2xl font-bold text-gray-900">Mentoria</h2>
-                </div>
-            </div>
-            <div className="space-y-4">
-                <TrainerView />
-            </div>
+  return (
+    <div className="space-y-8">
+      <div className="flex w-full gap-3 justify-between">
+        <div className="flex items-center gap-2">
+          <ListTodo className="h-5 w-5 text-blue-500" />
+          <h2 className="text-2xl font-bold text-gray-900">Mentoria</h2>
         </div>
-    );
+      </div>
+      <div className="space-y-4">
+        <TrainerView />
+      </div>
+    </div>
+  );
 }
