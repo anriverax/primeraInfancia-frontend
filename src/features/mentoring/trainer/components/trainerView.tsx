8395bd376eb6b59bd203189b9018b8d52858ac56
Jaime Pajares <jaimepajares@gmail.com>
2025-09-21 10:00:42 -0600

    feat get appendix resume

--- src/features/mentoring/trainer/components/trainerView.tsx
+++ src/features/mentoring/trainer/components/trainerView.tsx
@@ -1,87 +1,18 @@
-import { Eye, FileText, Users, BookOpen, ClipboardCheck, BarChart3, Target } from "lucide-react";
 import { Button, Card, CardBody } from "@heroui/react";
 import Link from "next/link";
-
-const attachmentList = [
-  {
-    id: 1,
-    attachmentName: "Anexo 1",
-    title: "Acuerdo de mentoría",
-    phase: "Al iniciar la formación",
-    order: "Anexo 1",
-    description: "Documento inicial que establece los términos y objetivos de la mentoría",
-    icon: FileText,
-    color: "bg-blue-50 text-blue-700 border-blue-200"
-  },
-  {
-    id: 2,
-    attachmentName: "Anexo 2",
-    title: "Formulario inicial",
-    phase: "Al iniciar la formación",
-    order: "Anexo 1",
-    description: "Documento inicial que establece los términos y objetivos de la mentoría",
-    icon: ClipboardCheck,
-    color: "bg-blue-50 text-blue-700 border-blue-200"
-  },
-  {
-    id: 3,
-    attachmentName: "Anexo 3",
-    title: "Registro de planificación y retroalimentación: plan de mejora",
-    phase: "Al finalizar la formación",
-    order: "Anexo 1",
-    description: "Documento inicial que establece los términos y objetivos de la mentoría",
-    icon: Target,
-    color: "bg-green-50 text-green-700 border-green-200"
-  },
-  {
-    id: 5,
-    attachmentName: "Anexo 5",
-    title: "Cuaderno de mentoría",
-    phase: "Al finalizar la formación",
-    order: "Anexo 1",
-    description: "Documento inicial que establece los términos y objetivos de la mentoría",
-    icon: BookOpen,
-    color: "bg-green-50 text-green-700 border-green-200"
-  },
-  {
-    id: 6,
-    attachmentName: "Anexo 6",
-    title: "Registro de visitas y seguimiento",
-    phase: "Al finalizar la formación",
-    order: "Anexo 1",
-    description: "Documento inicial que establece los términos y objetivos de la mentoría",
-    icon: Users,
-    color: "bg-green-50 text-green-700 border-green-200"
-  },
-  {
-    id: 7,
-    attachmentName: "Anexo 7",
-    title: "Informe final de mentor que entrega al personal técnico de apoyo",
-    phase: "Al finalizar la formación",
-    order: "Anexo 1",
-    description: "Documento inicial que establece los términos y objetivos de la mentoría",
-    icon: BarChart3,
-    color: "bg-green-50 text-green-700 border-green-200"
-  },
-  {
-    id: 8,
-    attachmentName: "Anexo 8",
-    title:
-      "Guía de observación y medición del grado de implementación del diseño curricular para la transformación de las prácticas docentes en la aulas de primera infancia de El Salvador",
-    phase: "Al finalizar la formación",
-    order: "Anexo 1",
-    description: "Documento inicial que establece los términos y objetivos de la mentoría",
-    icon: Eye,
-    color: "bg-green-50 text-green-700 border-green-200"
-  }
-];
+import useAxios from "@/shared/hooks/useAxios";
+import { useAppendixList } from "@/features/attachment/hooks/appendix/useAppendixList";
+import { Eye, FileText, Users, BookOpen, ClipboardCheck, BarChart3, Target } from "lucide-react";
 
 const TrainerView = (): React.JSX.Element => {
+  const useRequest = useAxios(true);
+  const { appendixsList } = useAppendixList();
+
   return (
     <div className="flex items-center gap-2">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {attachmentList.map((item) => {
-          const IconComponent = item.icon;
+        {appendixsList?.map((item) => {
+          const IconComponent = item.iconName;
           return (
             <Card
               key={item.id}
@@ -92,7 +23,7 @@ const TrainerView = (): React.JSX.Element => {
                   <div
                     className={`inline-flex items-center px-3 py-1 rounded-full text-xs font-medium border ${item.color}`}
                   >
-                    {item.phase} • {item.attachmentName}
+                    {item.periodicity} • {item.title}
                   </div>
                   {/* Indicador de la realización */}
                 </div>
