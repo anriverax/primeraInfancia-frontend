c591a3794c62ff081e6114e16a7804d19ca5b50a
Aníbal Rivera <anriverax@gmail.com>
2025-10-14 12:07:29 -0600

    update responsive

--- src/shared/ui/custom/tableLayout.tsx
+++ src/shared/ui/custom/tableLayout.tsx
@@ -2,7 +2,11 @@ import { ScrollShadow } from "@heroui/react";
 import { PropsWithChildren } from "react";
 
 export const TableLayout = ({ children }: PropsWithChildren): React.JSX.Element => (
-  <ScrollShadow orientation="horizontal" hideScrollBar={false} className="shadow-small rounded-large">
+  <ScrollShadow
+    orientation="horizontal"
+    hideScrollBar={false}
+    className="shadow-small rounded-large max-w-full"
+  >
     <div className="max-w-xs newQuery md:max-w-full lg:max-w-full">{children}</div>
   </ScrollShadow>
 );
