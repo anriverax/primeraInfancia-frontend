42566e579adc0c6edf58b598bb8309af52f76fcd
Jaime Pajares <jaimepajares@gmail.com>
2025-10-22 22:05:02 -0600

    fix show subtitle appendix

--- src/features/group/components/detail/table/groupDFM.tsx
+++ src/features/group/components/detail/table/groupDFM.tsx
@@ -12,13 +12,13 @@ const GroupDFM = (): React.JSX.Element => {
 
   const groupList = groupDetailList ?? [];
   groupList?.sort((a, b) => (a.Person?.fullName ?? "").localeCompare(b.Person?.fullName ?? ""));
-  let value = "2"
-  const idArray = groupList.map(item => item.id);
-  const { dashboardDetail } = useGetAnswer(idArray);
+  // let value = "2"
+  // const idArray = groupList.map(item => item.id);
+  // const { dashboardDetail } = useGetAnswer(idArray);
   
-  console.log(groupDetailList, "###");
+  // console.log(groupDetailList, "###");
 
-  if (value == "1")
+  // if (value == "1")
     return (
       <TableLayout>
         <Table
@@ -44,31 +44,31 @@ const GroupDFM = (): React.JSX.Element => {
         </Table>
       </TableLayout>
     );
-  else return (
-    <TableLayout>
-      <Table
-        removeWrapper
-        className="min-w-[max-content]"
-        classNames={tableClassNames}
-        aria-label="Tabla para mostrar las rutas de aprendizaje registradas"
-      >
-        <TableHeader columns={groupDetailByUserColumns}>
-          {(col) => <TableColumn key={col.key}>{col.label}</TableColumn>}
-        </TableHeader>
-        <TableBody isLoading={!dashboardDetail} items={dashboardDetail || []}>
-          {(groupDetailByUserItem: PersonAppendixDto) => (
-            <TableRow key={groupDetailByUserItem.Person.firstName}>
-              {(groupDetailByUserKey) => (
-                <TableCell>
-                  {renderGroupDFM(groupDetailByUserItem, groupDetailByUserKey as IGroupByUserColumnKey)}
-                </TableCell>
-              )}
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableLayout>
-  );
+  // else return (
+  //   <TableLayout>
+  //     <Table
+  //       removeWrapper
+  //       className="min-w-[max-content]"
+  //       classNames={tableClassNames}
+  //       aria-label="Tabla para mostrar las rutas de aprendizaje registradas"
+  //     >
+  //       <TableHeader columns={groupDetailByUserColumns}>
+  //         {(col) => <TableColumn key={col.key}>{col.label}</TableColumn>}
+  //       </TableHeader>
+  //       <TableBody isLoading={!dashboardDetail} items={dashboardDetail || []}>
+  //         {(groupDetailByUserItem: PersonAppendixDto) => (
+  //           <TableRow key={groupDetailByUserItem.Person.firstName}>
+  //             {(groupDetailByUserKey) => (
+  //               <TableCell>
+  //                 {renderGroupDFM(groupDetailByUserItem, groupDetailByUserKey as IGroupByUserColumnKey)}
+  //               </TableCell>
+  //             )}
+  //           </TableRow>
+  //         )}
+  //       </TableBody>
+  //     </Table>
+  //   </TableLayout>
+  // );
 };
 
 export default GroupDFM;
